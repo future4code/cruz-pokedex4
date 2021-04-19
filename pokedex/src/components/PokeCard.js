3b6bb9bf30b90f08cef7bf6e8a6e7ce819289e10
Paula Lopes <paulalopessantos28@gmail.com>
2021-04-19 16:25:14 -0300

    alteração ordem das pastas

---
+++ pokedex/src/components/PokeCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const CardContainer = styled.div``;
+const ButtonContainer = styled.div``;
+
+export const PokeCard = () => {
+  return( <CardContainer></CardContainer>;
+  <ButtonContainer><button></button></ButtonContainer>
+
+
+};
+export default PokeCard;
