b4fd3adce6782ae377db744ca895ee024517a0b4
Paula Lopes <paulalopessantos28@gmail.com>
2021-04-19 17:21:22 -0300

    Criação do pokecard

---
+++ pokedex/src/components/PokeCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const CardContainer = styled.div``;
+const ButtonContainer = styled.div``;
+
+export const PokeCard = () => {
+  return (
+    <>
+      <CardContainer></CardContainer>;
+      <ButtonContainer>
+        <button>Adicionar</button>
+        <button>Ver detalhes</button>
+      </ButtonContainer>
+    </>
+  );
+};
