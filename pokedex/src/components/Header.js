3f9d12cdad9ce2d33cebb8eea4732e0cc25f4834
Paula Lopes <paulalopessantos28@gmail.com>
2021-04-20 17:06:05 -0300

    criação da home

--- pokedex/src/components/Header.js
+++ pokedex/src/components/Header.js
@@ -1,35 +1,34 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 const Header = (props) => {
+  return (
+    <HeaderPage>
+      {props.button}
+      {props.title}
+      {props.button2}
+    </HeaderPage>
+  );
+};
 
-    return (
-        <HeaderPage>
-            {props.button}
-            {props.title}
-            {props.button2}
-        </HeaderPage>
-    )
-}
-
-export default Header
+export default Header;
 
 const HeaderPage = styled.header`
-height: 60px;
-background: blue;
-width: 100%;
-position: fixed;
-display: flex;
-align-items: center;
-justify-content: center;
+  height: 60px;
+  background: #d92525;
+  width: 100%;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-button:first-child{
+  button:first-child {
     position: absolute;
     left: 20px;
-}
+  }
 
-button:last-child{
+  button:last-child {
     position: absolute;
     right: 20px;
-}
-`
+  }
+`;
--- pokedex/src/components/PokeCard.js
+++ pokedex/src/components/PokeCard.js
@@ -1,14 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-const Container = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100vh;
-  box-sizing: border-box;
-  align-items: center;
-  justify-content: center;
-  background-color: green;
-`;
+
 const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -57,16 +49,13 @@ const Card = styled.div`
 export const PokeCard = () => {
   return (
     <>
-      <Container>
-        <CardContainer>
-          <Card></Card>
-          <ButtonContainer>
-            <button>Adicionar para pokedex</button>
-            <button>Ver detalhes</button>
-          </ButtonContainer>
-        </CardContainer>
-      </Container>
-      ;
+      <CardContainer>
+        <Card></Card>
+        <ButtonContainer>
+          <button>Adicionar para pokedex</button>
+          <button>Ver detalhes</button>
+        </ButtonContainer>
+      </CardContainer>
     </>
   );
 };
