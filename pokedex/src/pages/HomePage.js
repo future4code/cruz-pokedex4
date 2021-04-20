57df7630ea10b436a547eb3924022b7d221f7457
Angelo O'Dwyer <70985334+AngeloVSO@users.noreply.github.com>
2021-04-20 20:21:18 -0300

    testando as requisições

--- pokedex/src/pages/HomePage.js
+++ pokedex/src/pages/HomePage.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { goToPokedexPage } from "../Routes/Coordinator";
 import styled from "styled-components";
 import PokeCard from "../components/PokeCard";
 import Header from "../components/Header";
 import Pokedex from "../components/img/Pokedex.jpg";
+import axios from "axios"
 
 const GridCardContainer = styled.div`
   background-image: url(${Pokedex});
@@ -22,31 +23,47 @@ const GridCardContainer = styled.div`
 
 const HomePage = () => {
   const history = useHistory();
+
+  const [pokeName, setPokeName] = useState([])
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
+    .then(res => {
+      setPokeName(res.data.results)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
+  useEffect(() => {
+  const newArray = []    
+  pokeName.forEach((item) => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${item.name}`)
+    .then(res => {
+      newArray.push(res.data)
+      const ordenedArray = newArray.sort((a,b) => {
+        return a.id - b.id
+      })
+    setPokemons(ordenedArray)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  })
+  }, [pokeName])
+
+  console.log("data", pokemons)
+
   return (
     <div>
       <Header />
       <button onClick={() => goToPokedexPage(history)}>POKEDEX</button>
       <GridCardContainer>
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
-        <PokeCard />
+       {pokemons && pokemons.map(poke => {
+         return <PokeCard key={poke.id} pokemon={poke}/>
+       })}
       </GridCardContainer>
     </div>
   );
